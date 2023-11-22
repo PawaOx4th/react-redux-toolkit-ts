4286e433fb72bece27b2faf26ac19478d560a43f
Kaen Pawanachai <pawaox4th@gmail.com>
2023-11-22 23:42:42 +0700

    feat: 🎸 fetch data with fn() in AsyncTruck

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import "./App.css"
-import { onIncrement } from "./store/counter/index.slice"
+import { onIncrement, onFetchTodoById } from "./store/counter/index.slice"
 import { useAppDispatch, useAppSelector } from "./store/useStore"
 
 function App() {
-  const counter = useAppSelector((state) => state.counter.count)
+  const { count, todo, loading } = useAppSelector((state) => state.counter)
   const dispatch = useAppDispatch()
 
   const handleIncrement = (arg?: Parameters<typeof onIncrement>[0]): void => {
     dispatch(onIncrement(arg))
   }
+
   return (
     <>
       <div>
         <h1>Counter</h1>
-        <p>{counter}</p>
+        <p>{count}</p>
         <button
           onClick={() => handleIncrement()}
           className="bg-blue-400 px-8 py-2 rounded-md text-white text-2xl active:bg-blue-600 transition-all"
         >
           +
         </button>
+        <br />
+        <hr />
+        <br />
+        <button
+          className="bg-blue-400 px-8 py-2 rounded-md text-white text-2xl active:bg-blue-600 transition-all"
+          onClick={() => dispatch(onFetchTodoById(count))}
+        >
+          Fetch todo with ID
+        </button>
+        {loading ? (
+          <div>
+            <small>Loading...</small>
+          </div>
+        ) : (
+          <div>
+            <pre>
+              <code>{JSON.stringify(todo, null, 2)}</code>
+            </pre>
+          </div>
+        )}
       </div>
     </>
   )
--- src/store/counter/index.slice.ts
+++ src/store/counter/index.slice.ts
@@ -1,16 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import type { PayloadAction } from "@reduxjs/toolkit"
 import { RootState } from "../store"
 
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+  userId: number
+}
 interface CounterState {
   count: number
   loading: boolean
+  todo: Todo | null
 }
 
 const initialState: CounterState = {
   count: 0,
   loading: false,
+  todo: null,
 }
+
+export const onFetchTodoById = createAsyncThunk(
+  "counter/fetchTodoById",
+  async (id: number) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    )
+    const todo = await response.json()
+    return todo as Todo
+  }
+)
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -24,7 +43,18 @@ export const counterSlice = createSlice({
       state.count += action.payload
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(onFetchTodoById.fulfilled, (state, action) => {
+      state.loading = false
+      state.count = action.payload.id
+      state.todo = action.payload
+    }),
+      builder.addCase(onFetchTodoById.pending, (state) => {
+        state.loading = true
+      })
+  },
 })
 
 export const { onIncrement } = counterSlice.actions
+
 export const counterSelector = (state: RootState) => state.counter
