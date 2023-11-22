969427a17377b03b677db04bf77552cea5a47d92
Kaen Pawanachai <pawaox4th@gmail.com>
2023-11-22 22:51:51 +0700

    feat: 🎸 create store

---
+++ src/store/counter/index.slice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit"
+import type { PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "../store"
+
+interface CounterState {
+  count: number
+  loading: boolean
+}
+
+const initialState: CounterState = {
+  count: 0,
+  loading: false,
+}
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    onIncrement: (state, action: PayloadAction<number>) => {
+      state.count += action.payload
+    },
+  },
+})
+
+export const { onIncrement } = counterSlice.actions
+export const counterSelector = (state: RootState) => state.counter
